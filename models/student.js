afa53f104bcf656ff533a62b9250f2a74593e539
Arunav Gupta <arunav1101@gmail.com>
2019-06-14 00:24:33 -0700

    added server file and schemas

---
+++ models/student.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  urlGithub: String,
+  urlImage: String,
+  date: { type: Date, default: Date.now }
+});
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;
