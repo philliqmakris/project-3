071f454cd691049c9ad997f8b3bc78d08e0a57ee
Nash McDonald <nash42192@gmail.com>
2019-06-15 12:34:28 -0700

    committing changes

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <>
-        <Banner />
+        {/* <Banner /> */}
         <Navbar />
         <Wrapper>
         <Route exact path="/" component={Main} />
