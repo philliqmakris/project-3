0a064235d7de964e33c5c530ac711811b39fcd4c
Arunav Gupta <arunav1101@gmail.com>
2019-06-16 03:30:44 -0700

    minor changes, removed console logs

--- client/src/Passport/passport.js
+++ client/src/Passport/passport.js
@@ -2,7 +2,7 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const db = require('../../../models/user');
 
 
-module.exports = function(passport) {
+module.exports = (passport)=> {
 	passport.use(
 		new GoogleStrategy(
 			{
@@ -10,8 +10,7 @@ module.exports = function(passport) {
 				clientSecret: 'u6n9kYJHuphxfmjCX2Rql6Vh',
 				callbackURL: '/auth/google/redirect'
 			},
-			function(accessToken, refreshToken, profile, done) {
-                console.log('tets',profile);
+			(accessToken, refreshToken, profile, done) =>{
                 const newUser = {
 					GoogleID: profile.id,
 					firstName: profile.name.givenName,
@@ -19,27 +18,13 @@ module.exports = function(passport) {
 					emailAddress: profile.emails[0].value,
 					photo: profile.photos[0].value
 				};
-
-console.log('myuser',newUser)
-				//* (accessToken,refreshToken,profile)=>{
-
-			// }
-		//db.inventory.find( { qty: { $eq: 20 } } )
+				
 				db
-					.find({ GoogleID:{$eq: newUser.GoogleID
-						}
+					.find({ GoogleID:{$eq: newUser.GoogleID}
 					})
 					.then((user) => {
-						console.log('Result',user)
-						if (user[0]) {
-							//return USER
-							done(null, newUser);
-					
-						} else {
-							db.create(newUser).then((user) => {
-								done(null, user);
-							});
-						}
+						if (user[0]) done(null, newUser);
+						else db.create(newUser).then((user) =>done(null, user));
 					})
 					.catch((err) => console.log(err));
 			}
