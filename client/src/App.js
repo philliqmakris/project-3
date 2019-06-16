5d126c7d9b97df959ef0d27011cf627180b250a4
Arunav Gupta <arunav1101@gmail.com>
2019-06-16 02:47:15 -0700

    remove unwanted code

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,6 @@ import Profile from './pages/Profile';
 import Navbar from './components/Navbar/index.js';
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
-import Banner from "./components/Banner/index.js";
-import NotFound from "./components/Notfound/index.js";
-// import Passport from "../src/Passport/passport";
 import './App.css';
 
 function App() {
@@ -22,9 +19,6 @@ function App() {
         <Route exact path="/profiles" component={Profile} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        {/* <Route exact path="/" component={LoginGoogle} /> */}
-        {/* <Route exact path="/auth/google" component={Passport} /> */}
-        {/* <Route exact path="*" component={NotFound} /> */}
         </Wrapper>
         <Footer />
       </>
