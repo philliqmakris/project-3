db8221f94a16c9f11779a82be9e512744c41e9e6
Nash McDonald <nash42192@gmail.com>
2019-06-24 11:55:28 -0700

    added enginses to server package.json file

---
+++ client/build/precache-manifest.05f524f5cd45873cfb52278545979e5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cee5fdfe86705012b80",
+    "url": "/static/css/main.de8ff618.chunk.css"
+  },
+  {
+    "revision": "9cee5fdfe86705012b80",
+    "url": "/static/js/main.cbdf21e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a2f81eec8d0b78ca677f",
+    "url": "/static/js/2.c90a5ea0.chunk.js"
+  },
+  {
+    "revision": "41f131ddc6beda2af2b16e241e9f23c5",
+    "url": "/index.html"
+  }
+];
