edc7f22e63ae49e311ba08734bed6780f8fb7653
Nash McDonald <nash42192@gmail.com>
2019-06-24 12:24:44 -0700

    fixed hanging curly bracket in index of Profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -7,75 +7,70 @@ import Sidenav from "../../components/Sidenav";
 class Profile extends Component {
 
   state = {
-    stud:[],
-    loggedUser:[],
-    filterUser:[]
-
-  };
+    stud: [],
+    loggedUser: [],
+    filterUser: []
+  }
 
   componentDidMount() {
     this.loadStudents();
-    this.getUserDetails(); 
-    this.filterLoggedUser();     
-    }
-
-    getUserDetails=async ()=>{
-      const loggedInUserId = window.location.toString().split("/").pop().split('#')[0];
-      await API.getUserByGoogleId(loggedInUserId)
-      .then(async (res) =>{
-        await this.setState({ loggedUser: res.data })
-       
-
-      })
-    }
-  
-     loadStudents = () => {  
-      API.getStudents()
-        .then(async (res) =>{
-          await this.setState({ stud: res.data })
-          await this.props.isAuthenticated(true)
-        }
-        )
-        .catch(err => console.log(err));
-    };
-
+    this.getUserDetails();
+    this.filterLoggedUser();
   }
 
-  deleteStudent = ({ target }) => {
-    const { dataset } = target;
-    API.deleteStudent(dataset.id)
-      .then(res => this.loadStudents())
+  getUserDetails = async () => {
+    const loggedInUserId = window.location.toString().split("/").pop().split('#')[0];
+    await API.getUserByGoogleId(loggedInUserId)
+      .then(async (res) => {
+        await this.setState({ loggedUser: res.data })
+      });
+  }
+
+  loadStudents = () => {
+    API.getStudents()
+      .then(async (res) => {
+        await this.setState({ stud: res.data })
+        await this.props.isAuthenticated(true)
+      }
+      )
       .catch(err => console.log(err));
   };
 
-  filterLoggedUser= async ()=>{
-   await this.getUserDetails();
-const userFilter =(this.state.loggedUser.map(userdetails=>userdetails.firstName +' '+ userdetails.lastName ));
-this.setState({ filterUser: userFilter[0].toLocaleLowerCase().trim() })
-  }
-  
-  render() {
+
+
+deleteStudent = ({ target }) => {
+  const { dataset } = target;
+  API.deleteStudent(dataset.id)
+    .then(res => this.loadStudents())
+    .catch(err => console.log(err));
+};
+
+filterLoggedUser = async () => {
+  await this.getUserDetails();
+  const userFilter = (this.state.loggedUser.map(userdetails => userdetails.firstName + ' ' + userdetails.lastName));
+  this.setState({ filterUser: userFilter[0].toLocaleLowerCase().trim() })
+}
+
+render() {
 
   return (
-      <div className="d-flex flex-row">
-                    <div className="d-flex flex-column align-self-center mr-5">
-                         <Sidenav loggedUserDetails={this.state.loggedUser}/>   
-                     </div>
-                     <div className="d-flex flex-column mt-5">
-                        
-                         <div className="row text-center mt-5" id="cardDiv">
-                         
-                           <ProfileCard 
-                           results={this.state.stud.filter(
-                             (myfilter)=>
-                                myfilter.name.toLocaleLowerCase().trim()!==this.state.filterUser)} 
-                                />
+    <div className="d-flex flex-row">
+      <div className="d-flex flex-column align-self-center mr-5">
+        <Sidenav loggedUserDetails={this.state.loggedUser} />
+      </div>
+      <div className="d-flex flex-column mt-5">
 
-</div>
-</div>
-</div>
-    )
-  }
+        <div className="row text-center mt-5" id="cardDiv">
+          <ProfileCard
+            results={this.state.stud.filter(
+              (myfilter) =>
+                myfilter.name.toLocaleLowerCase().trim() !== this.state.filterUser)}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
 }
 
 export default Profile;
