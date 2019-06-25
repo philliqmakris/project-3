1e7e93a9a1724457374edb7d3c1c5123685309ef
Nash McDonald <nash42192@gmail.com>
2019-06-24 18:28:57 -0700

    added different server host OR for testing

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 
 const serverHost = process.env.serverHost || "http://localhost:";
 const PORT = process.env.PORT || 3000;
+const testHost = process.env.serverHost || "http://localhost:3000"
 
 console.log(serverHost);
 console.log(PORT);
@@ -19,11 +20,13 @@ route.get(
 	'/google/redirect',
 	passport.authenticate('google', {
 		//failureRedirect: "http://localhost:3000"
-		failureRedirect: serverHost+PORT
+		//failureRedirect: serverHost+PORT
+		failureRedirect: testHost
 	}),
 	function(req, resp) {
 		//resp.redirect("http://localhost:3000/Profiles/"+ req.user.GoogleID);
-		resp.redirect(serverHost+PORT+"/Profiles/"+ req.user.GoogleID);
+		//resp.redirect(serverHost+PORT+"/Profiles/"+ req.user.GoogleID);
+		resp.redirect(testHost+"/Profiles/"+ req.user.GoogleID);
 	}
 );
 
