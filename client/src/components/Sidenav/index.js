f9e057ad750c5eac93428c5d336fe2f9e844a685
Nash McDonald <nash42192@gmail.com>
2019-06-29 17:39:35 -0700

    fixed navbar mobile formatting, made sideNav larger on open

--- client/src/components/Sidenav/index.js
+++ client/src/components/Sidenav/index.js
@@ -13,8 +13,8 @@ function closeNav() {
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "40vh";
+    document.getElementById("main").style.marginLeft = "40vh";
     document.getElementById("hamburger").style.display = "none";
   }
 
